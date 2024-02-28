a828fbfb24921e3c0a811295d64c9b4141b70a97
Isha Saini <ishasaini562@gmail.com>
2024-02-28 10:51:45 +0530

    first commit

---
+++ src/components/Review.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./review.css"
+
+const Review = () =>{
+    return(
+        <div className="component9">
+            <div className="custmer1">
+                <div className="custmer2"><i class="fa-solid fa-users"></i>
+                <h2 className="custmer3">SATISFIED CUSTOMERS</h2>
+                <h1>1963</h1>
+                </div>
+                <div className="custmer2"><i class="fa-solid fa-users"></i>
+                <h2 className="custmer3">QUALITY OF SERVICE</h2>
+                <h1>99%</h1>
+                </div>
+                <div className="custmer2"><i class="fa-solid fa-users"></i>
+                <h2 className="custmer3">QUALITY CERTIFICATES</h2>
+                <h1>33</h1>
+                </div>
+                <div className="custmer2"><i class="fa-solid fa-users"></i>
+                <h2 className="custmer3">AVAILABLE PRODUCTS</h2>
+                <h1>789</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Review
---
+++ src/components/Testimonial.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./testimonial.css"
+
+const Testimonial = () =>{
+    return(
+        <div className="component10">
+            <div className="test">
+            <h2>Our Testimonial</h2>
+            </div>
+            <div className="test1">
+            <h1>Our Client Saying!</h1>
+            </div>
+            <div className="test2">
+            <div className="btn1">
+            <i class="fa-solid fa-arrow-right-long"></i>
+            </div>
+            <div className="btn2">
+            <i class="fa-solid fa-arrow-left-long"></i>    
+            </div>
+            </div>
+            <div className="client">
+                <div className="client1">Lorem Ipsum is simply dummy text of the printing Ipsum has been the industry's
+                 standard dummy text ever since the 1500s,
+                 <hr />
+                 <div className="pick1">
+                    <div className="pick1">
+                        <h3>Client Name</h3>
+                        <p className="reviews-profession">Profession</p>
+                        <p>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        </p>
+                        <div className="icon"><i class="fa-solid fa-quote-right"></i></div>
+                    </div>
+                </div>
+                
+            </div>
+            
+                
+
+
+
+
+                <div className="client2">Lorem Ipsum is simply dummy text of the printing Ipsum has been the industry's
+                 standard dummy text ever since the 1500s,
+                 <hr />
+                 <div className="pick1">
+                    <div className="pick1">
+                        <h3>Client Name</h3>
+                        <p className="reviews-profession">Profession</p>
+                        <p>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        </p>
+                        <div className="icon1"><i class="fa-solid fa-quote-right"></i></div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+export default Testimonial
