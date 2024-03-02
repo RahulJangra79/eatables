5245e34662bd6dbd38436f6bcf30618afea04afb
Sheetal Yadav <sheetalyadav81683@gmail.com>
2024-03-02 11:32:23 +0530

    new file

---
+++ src/components/data/Bestseller_map.js
@@ -0,0 +1,65 @@
+export const best_product = [
+    {
+        name: "product-rating-name fa-solid fa-star",
+        star: "product-rating-star",
+        price: "product-rating-price",
+        button: "product-rating-buy-now-btn fa-solid fa-cart-shopping"
+    },
+    {
+        name: "product-rating-name fa-solid fa-star",
+        star: "product-rating-star",
+        price: "product-rating-price",
+        button: "product-rating-buy-now-btn fa-solid fa-cart-shopping"
+    },
+    {
+        name: "product-rating-name fa-solid fa-star",
+        star: "product-rating-star",
+        price: "product-rating-price",
+        button: "product-rating-buy-now-btn fa-solid fa-cart-shopping"
+    },
+    {
+        name: "product-rating-name fa-solid fa-star",
+        star: "product-rating-star",
+        price: "product-rating-price",
+        button: "product-rating-buy-now-btn fa-solid fa-cart-shopping"
+    },
+    {
+        name: "product-rating-name fa-solid fa-star",
+        star: "product-rating-star",
+        price: "product-rating-price",
+        button: "product-rating-buy-now-btn fa-solid fa-cart-shopping"
+    },
+    {
+        name: "product-rating-name fa-solid fa-star",
+        star: "product-rating-star",
+        price: "product-rating-price",
+        button: "product-rating-buy-now-btn fa-solid fa-cart-shopping"
+    },
+]
+
+export const best_product_last = [
+    {
+        name: "bestseller-second-component-image1-des-name",
+        star: "bestseller-second-component-image1-des-star",
+        price: "bestseller-second-component-image1-des-price",
+        button: "bestseller-second-component-image1-des-buy-btn"
+    },
+    {
+        name: "bestseller-second-component-image1-des-name",
+        star: "bestseller-second-component-image1-des-star",
+        price: "bestseller-second-component-image1-des-price",
+        button: "bestseller-second-component-image1-des-buy-btn"
+    },
+    {
+        name: "bestseller-second-component-image1-des-name",
+        star: "bestseller-second-component-image1-des-star",
+        price: "bestseller-second-component-image1-des-price",
+        button: "bestseller-second-component-image1-des-buy-btn"
+    },
+    {
+        name: "bestseller-second-component-image1-des-name",
+        star: "bestseller-second-component-image1-des-star",
+        price: "bestseller-second-component-image1-des-price",
+        button: "bestseller-second-component-image1-des-buy-btn"
+    },
+]
