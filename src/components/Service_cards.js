b908d4026415eb260d7070233aa27dae7fbf78ba
RahulJangra <rjangra4015@gmail.com>
2024-03-02 11:34:50 +0530

    implementing map

--- src/components/Service_cards.js
+++ src/components/Service_cards.js
@@ -1,31 +1,18 @@
 import React from "react";
 import "./Service_cards.css"
-
+import {service} from "./data/ServiceCardContent.js";
 const Service_card = () => {
+    console.log("data=>", service)
     return(
         <div className="service-card-outer">
             <div className="service-detail-card">
-                <div className="service-detail-card-item">
-                    <i class="fa-solid fa-car-side"></i>
-                    <div className="rotated-square"></div>
-                    <h5 className="heading-5-service-card">Free Shipping</h5>
-                    <p className="paragraph-service-card">Free on order over $100</p>
-                </div>
-                <div className="service-detail-card-item">
-                    <i class="fa-solid fa-user-shield"></i>
-                    <h5 className="heading-5-service-card">Security Payment</h5>
-                    <p className="paragraph-service-card">100% secure payment</p>
-                </div>
-                <div className="service-detail-card-item">
-                    <i class="fa-solid fa-clock"></i>
-                    <h5 className="heading-5-service-card">30 Min Delivery</h5>
-                    <p className="paragraph-service-card">Guaranteed delivery on time</p>
-                </div>
-                <div className="service-detail-card-item">
-                    <i class="fa-solid fa-phone"></i>
-                    <h5 className="heading-5-service-card">24/7 Support</h5>
-                    <p className="paragraph-service-card">Support every time fast</p>
-                </div>
+                {service.map((item,index)=>{
+                  return ( <div key={index} className="service-detail-card-item">
+                    <i class={`${item.icon}`}></i>
+                    <h5 className="heading-5-service-card">{item.title}</h5>
+                    <p className="paragraph-service-card">{item.paragrph}</p>
+                </div>)
+                })}
             </div>
         </div>
     )
---
+++ src/components/data/ServiceCardContent.js
@@ -0,0 +1,22 @@
+export const service = [
+    {
+    icon: "fa-solid fa-car-side",
+    title: "Free Shipping",
+    paragrph:"Free on order over $100"
+    },
+    {
+    icon: "fa-solid fa-user-shield",
+    title: "Security Payment",
+    paragrph:"100% secure payment"
+    },
+    {
+    icon: "fa-solid fa-clock",
+    title: "30 Min Delivery",
+    paragrph:"Guaranteed delivery on time"
+    },
+    {
+    icon: "fa-solid fa-phone",
+    title: "24/7 Support",
+    paragrph:"Support every time fast"
+    },
+]
