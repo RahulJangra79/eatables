dc12ecc0186e96e824a04e9b849f29023c59d6b2
RahulJangra <rjangra4015@gmail.com>
2024-03-10 10:25:09 +0530

    applying map and adjust service card content

--- src/components/Component5.js
+++ src/components/Component5.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Component5.css"
+import { component5Data } from "./data/Component5Data";
 
 const Component5 = () => {
+
     return(
         <div className="component-5">
             <div className="bigger-products">
                 <div className="bigger-product-items">
-                    <div className="bigger-product-item">
+                    {component5Data.map((item, index)=>{
+                        return(
+                            <div className="bigger-product-item">
+                                <div className="bigger-product-item-image" id={item.imageid}></div>
+                                <div className="bigger-product-solid-color-orange"></div>
+                                <div className="bigger-product-item-center-part">
+                                    <h3>{item.name}</h3>
+                                    <p>{item.offer}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    {/* <div className="bigger-product-item">
                         <div className="bigger-product-item-image" id="fries-11"></div>
                         <div className="bigger-product-solid-color-orange"></div>
                         <div className="bigger-product-item-center-part">
@@ -29,7 +43,7 @@ const Component5 = () => {
                             <h3>Fry Momos</h3>
                             <p>$ 1.99 Discount</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
---
+++ src/components/data/Component5Data.js
@@ -0,0 +1,17 @@
+export const component5Data = [
+    {
+        imageid: "fries-11",
+        name: "French Fries",
+        offer: "20% OFF"
+    },
+    {
+        imageid: "kachori-22",
+        name: "Special kachori",
+        offer: "Free Delivery"
+    },
+    {
+        imageid: "fry-momos-33",
+        name: "Fry Momos",
+        offer: "$ 1.99 Discount"
+    },
+]
