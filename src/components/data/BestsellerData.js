4775242f75bdc6ede8e256a96f76680478b00361
Sheetal Yadav <sheetalyadav81683@gmail.com>
2024-03-11 11:52:11 +0530

    changes

---
+++ src/components/data/BestsellerData.js
@@ -0,0 +1,56 @@
+export const bestsellerData = [
+    {
+        imageclass: "kachori-photo",
+        name: "Kachori",
+        price: "4.79 $"
+    },
+    {
+        imageclass: "fries-photo",
+        name: "French Fries",
+        price: "5.99 $"
+    },
+    {
+        imageclass: "manchurian-photo",
+        name: "Manchurian",
+        price: "7.89 $"
+    },
+    {
+        imageclass: "noodles-photo",
+        name: "Noodles",
+        price: "5.89 $"
+    },
+    {
+        imageclass: "springroll-photo",
+        name: "Spring Roll",
+        price: "6.99 $"
+    },
+    {
+        imageclass: "dahi-golgappe-photo",
+        name: "Dahi Golgappe",
+        price: "5.15 $"
+    }
+]
+
+
+export const bestsellerData_second = [
+    {
+        imageclass: "bestseller-second-component-image1",
+        name: "Pizza",
+        price: "15.99 $"
+    },
+    {
+        imageclass: "bestseller-second-component-image2",
+        name: "Steam Momos",
+        price: "7.50$"
+    },
+    {
+        imageclass: "bestseller-second-component-image3",
+        name: "Pani Golgappe",
+        price: "4.79"
+    },
+    {
+        imageclass: "bestseller-second-component-image4",
+        name: "Samosa",
+        price: '6.89 $'
+    }
+]
