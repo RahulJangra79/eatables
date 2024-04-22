00e6ca5bd3cf2a3c8092eb841d7bfc26c9d6f545
SheetalYadav79 <sheetalyadav81683@gmail.com>
2024-04-22 12:27:39 +0530

    update

--- src/components/ManageProducts.js
+++ src/components/ManageProducts.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 const ManageProducts = () => {
     const [data, setData] = useState();
+    const [editing, setEditing] = useState(null);
+
 
     useEffect(() => {
         axios.get("http://localhost:5000/products")
@@ -16,6 +18,23 @@ const ManageProducts = () => {
             });
     }, []);
 
+    const handleEdit = (item) => {
+        setEditing(item);
+      };
+    
+      const handleUpdate = (item) => {
+        axios.put(`http://localhost:5000/products/${item._id}`, item)
+          .then((response) => {
+            console.log("Updated Data =>", response.data);
+            setData(response.data.products);
+            setEditing(null);
+          })
+          .catch((err) => {
+            console.log("Error =>", err);
+          });
+      };
+    
+
     return (
         <div className='manage-products'>
             <h1 className='manage-products-heading'>Manage Products</h1>
@@ -25,7 +44,6 @@ const ManageProducts = () => {
                         <th>Name</th>
                         <th>Price</th>
                         <th>Description</th>
-                        <th>Type</th>
                         <th>Status</th>
                         <th>Discount</th>
                         <th>Rating</th>
@@ -39,12 +57,11 @@ const ManageProducts = () => {
                             <td>{item.name}</td>
                             <td>{item.price}</td>
                             <td>{item.description}</td>
-                            <td>{item.type}</td>
                             <td>{item.status}</td>
                             <td>{item.discount}</td>
                             <td>{item.rating}</td>
                             <td className='manage-products-icons'>
-                              <button><i class="fa-solid fa-pen-to-square"></i></button>
+                              <button onClick={() => handleEdit(item)}><i class="fa-solid fa-pen-to-square"></i></button>
                               <button><i class="fa-solid fa-trash"></i></button>
                             </td>
                         </tr>
@@ -52,6 +69,56 @@ const ManageProducts = () => {
                     })}
                 </tbody>
             </table>
+
+            {editing && (
+        <div>
+          <h2>Edit Product</h2>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            handleUpdate(editing);
+          }}>
+            <label>
+              Name:
+              <input type="text" value={editing.name} onChange={(e) => {
+                setEditing({ ...editing, name: e.target.value });
+              }} />
+            </label>
+            <label>
+              Price:
+              <input type="number" value={editing.price} onChange={(e) => {
+                setEditing({ ...editing, price: e.target.value });
+              }} />
+            </label>
+            <label>
+              Description:
+              <input type="text" value={editing.description} onChange={(e) => {
+                setEditing({ ...editing, description: e.target.value });
+              }} />
+            </label>
+            <label>
+              Status:
+              <input type="text" value={editing.status} onChange={(e) => {
+                setEditing({ ...editing, status: e.target.value });
+              }} />
+            </label>
+            <label>
+              Discount:
+              <input type="number" value={editing.discount} onChange={(e) => {
+                setEditing({ ...editing, discount: e.target.value });
+              }} />
+            </label>
+            <label>
+              Rating:
+              <input type="number" value={editing.rating} onChange={(e) => {
+                setEditing({ ...editing, rating: e.target.value });
+              }} />
+            </label>
+            <button type="submit">Update</button>
+          </form>
+        </div>
+      )}
+
+
         </div>
     );
 }
